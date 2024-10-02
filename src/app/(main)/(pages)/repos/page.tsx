b04504c9dd5cbc94effedca879783a8b41d3d0d6
jagadeesh m <jagadeeshm2002@gmail.com>
2024-10-02 18:21:11 +0530

    .

--- src/app/(main)/(pages)/repos/page.tsx
+++ src/app/(main)/(pages)/repos/page.tsx
@@ -7,9 +7,9 @@ type Props = {};
 export default function ReposPage({}: Props) {
   return (
     <div className="flex flex-col">
-      <div className="flex flex-row justify-between">
+      <div className="flex flex-row justify-between mb-2">
         <div>Your Repos</div>
-        <div>
+        <div >
           <Link href={"/repos/new"} className="flex items-center" passHref={true}>
             <Button variant="ghost" className="px-4 outline outline-2">
               New
@@ -18,7 +18,11 @@ export default function ReposPage({}: Props) {
           </Link>
         </div>
       </div>
-      <div>REpo List</div>
+      <hr className="opacity-35"/>
+
+      <div className=" flex w-full flex-col mt-3">
+        <div className="w-full border border-gray-400 border-opacity-20 rounded-lg p-3">list</div>
+      </div>
     </div>
   );
 }
